08ae9ab06acac846494a6eaf81f873590e68e001
MJawad703 <m.jawad3300@gmail.com>
2022-05-10 22:49:54 +0500

    added styles of todo-list-items

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,14 @@
 const addTodo = document.getElementById("add-todo");
 const todoList = document.getElementById("todo-list");
+const todoInput = document.getElementById("todo-input");
+
 addTodo.addEventListener("click", (e) => {
   e.preventDefault();
   const newTodo = document.createElement("div");
   newTodo.classList.add("todo-list-item");
   // <div></div>
   const newTodoText = document.createElement("h5");
-  newTodoText.innerText = "ABC";
+  newTodoText.innerText = todoInput.value;
   newTodo.appendChild(newTodoText);
   // <div>New Todo</div>
   // <div class="todo-list-item">New Todo</div>
@@ -21,6 +23,7 @@ addTodo.addEventListener("click", (e) => {
   deleteButton.innerHTML = '<i class="fa-solid fa-trash"></i>';
   newTodo.appendChild(deleteButton);
   todoList.appendChild(newTodo);
+  todoInput.value = "";
   // <div id="todo-list">
   // <div class="todo-list-item">New Todo</div>
   // <div class="todo-list-item">New Todo</div>
@@ -29,4 +32,9 @@ addTodo.addEventListener("click", (e) => {
   // <div class="todo-list-item">New Todo</div>
   // <div class="todo-list-item">New Todo</div>
   // </div>
+});
+
+todoList.addEventListener("click", (e) => {
+  const parentElement = e.target.parentElement;
+  parentElement.remove();
 });
